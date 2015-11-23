09cedfd8b29174b90e64d5f8097edca4f50c6d4d
hath995 <hathlesstroubles@gmail.com>
2015-11-23 12:52:05 -0800

    updates to the library

--- src/series.ts
+++ src/series.ts
@@ -1,12 +1,38 @@
 
 export namespace JSPandas {
     "use strict";
+    function range(start, end) {
+        let result = [];
+        for(let i = start; i < end; i++) {
+            result.push(i);
+        }
+        return result;
+    }
+
     export class Series<A, B> {
-        data: A;
-        data_index: B;
-        constructor(data: A, index?: B) {
+        data: Array<A>;
+        data_index: Array<B>;
+        constructor(data: Array<A>, index?: Array<B>) {
             this.data = data;
+            if(index && index.length !== data.length) {
+                throw new Error("Wrong number of items passed in data");
+            }else if(index === undefined) {
+                index = range(0, data.length);
+            }
             this.data_index = index;
+        }
+
+        copy(deep: boolean) {
+            if(deep) {
+                return new Series(this.data.slice(), this.data_index.slice());
+            }else{
+                return new Series(this.data, this.data_index);
+            }
+        }
+
+        isNull() {
+            let values = this.data.map( val => val === null || val === undefined); 
+            return new Series(values, this.data_index);
         }
     }
 }
--- test/series.ts
+++ test/series.ts
@@ -6,6 +6,26 @@ describe('JSPandas.Series', () => {
     it('Should accept just an array', () => {
         let test = new JSPandas.Series([1,2,3,4]);
         expect(test.data).to.deep.equal([1,2,3,4]);
+        expect(test.data_index).to.deep.equal([0,1,2,3]);
+    });
+
+    it('Should only allow indices of matching size', () => {
+        expect(() => new JSPandas.Series([1],[0,2])).to.throw(Error);
+        expect(() => new JSPandas.Series([1,1],[0])).to.throw(Error);
+    });
+
+    it('should copy a series', () => {
+        let test = new JSPandas.Series([1,2,3,4]);
+        let shallow_copy = test.copy(false);
+        let deep_copy = test.copy(true);
+        expect(shallow_copy.data).to.equal(test.data);
+        expect(deep_copy.data).to.not.equal(test.data);
+    });
+
+    it('should create a new boolean series based on if values are null', () => {
+        let test = new JSPandas.Series([null, undefined, false, true]);
+        let isnull = test.isNull();
+        expect(isnull.data).to.deep.equal([true, true, false, false]);
     });
 });
 
